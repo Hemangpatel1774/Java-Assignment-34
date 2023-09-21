6002929b0a6732b2922ca20d1fae7a2d3817d44f
Hemangpatel1774 <patelhemang1774@gmail.com>
2023-09-21 20:29:13 +0530

    Roll_no_34

--- Assignment-4/Main4.java
+++ Assignment-4/Main4.java
@@ -1,12 +1,12 @@
 class ex2 {
 }
-public class Main{
-    Main(String name , int id){
+public class Main4{
+    Main4(String name , int id){
         System.out.println("Name : "+name);
         System.out.println("Id   : "+id);
     }
     public static void main(String[] args){
-        Main obj = new Main("Hemang",34);
+        Main4 obj = new Main4("Hemang",34);
     }
     }
 
